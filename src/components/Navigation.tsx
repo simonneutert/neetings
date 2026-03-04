259bffa6b1e1d980267c3d63afcbbb4c1dc2fbf7
Simon Neutert <simonneutert@users.noreply.github.com>
2026-03-04 17:54:46 +0100

    removes link to github

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -139,16 +139,6 @@ export function Navigation(
             </li>
           </ul>
           <div class="d-flex gap-2">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a
-                  class="nav-link"
-                  href="https://github.com/simonneutert/neetings-feedback-public-alpha/discussions"
-                >
-                  {t("navigation.github_discussion")}
-                </a>
-              </li>
-            </ul>
             <ThemeToggle />
             <LanguageSwitcher />
           </div>
